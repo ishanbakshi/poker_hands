e09f98699497cfa405a929059b07b487db615657
ishan bakshi <ishan.bakshi@gmail.com>
2014-12-31 14:18:40 +1100

    integration test for 2nd rule of poker

--- spec/poker_hands_spec.rb
+++ spec/poker_hands_spec.rb
@@ -1,17 +1,16 @@
 require 'spec_helper'
 describe 'Poker Hands' do
 
-=begin
   describe 'integration' do
 
     it 'should work with highest card' do
       poker_game = PokerGame.new
       expect(poker_game.winning_hand("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH")).to include('White')
       expect(poker_game.winning_hand("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C QH")).to include('Black')
+      expect(poker_game.winning_hand("Black: 2H 3D 2S 9C KD  White: 2C 3H 4S 8C QH")).to include('Black')
+      expect(poker_game.winning_hand("Black: 2H 3D 5S 9C 5D  White: 2C KH 4S 8C KH")).to include('White')
     end
-
   end
-=end
   it 'should return winning hand with highest card' do
     poker_game = PokerGame.new
     black_hand = instance_double('Hand',  name: 'Black',pair: 'no_pair')

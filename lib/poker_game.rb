6401610122bbb959228532c4b0f676cc420b19e3
ishan bakshi <ishan.bakshi@gmail.com>
2015-01-02 15:07:09 +1100

    poker hand with a triplet wins

--- lib/poker_game.rb
+++ lib/poker_game.rb
@@ -3,11 +3,14 @@ class PokerGame
   def winning_hand(both_hands)
     hands = HandsParser.parse(both_hands) 
     hand_eval_obj = HandsEvaluator.new
-    winning_hand_value = hand_eval_obj.card_with_pair(hands[0], hands[1])
-  if(winning_hand_value == false)         
+    winning_hand_value = hand_eval_obj.card_with_triplet(hands[0], hands[1])
+    if(winning_hand_value == false)         
+      winning_hand_value = hand_eval_obj.card_with_pair(hands[0], hands[1])
+    end
+    if(winning_hand_value == false)         
       winning_hand_value = HandsEvaluator.high_card(hands[0], hands[1])
-  end
-      winning_hand_value.name
+    end
+    winning_hand_value.name
    end
 
 end
